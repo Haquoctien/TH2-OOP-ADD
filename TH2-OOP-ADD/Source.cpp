3f168c10e136516bd6dafa55ea40d8bc5c381ab8
Ha Quoc Tien <17521122@gm.uit.edu.vn>
2018-03-19 15:55:43 +0700

    Add main function

---
+++ TH2-OOP-ADD/Source.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "BangGiaPhiBocXep.h"
+#include "BangGiaPhiThueXe.h"
+#include "DanhSachLoaiXe.h"
+
+int main(void)
+{
+	BangGiaPhiThueXe bangGiaCuaToi;
+	bangGiaCuaToi.nhap();
+	bangGiaCuaToi.xuat();
+	system("pause");
+	return 0;
+}
